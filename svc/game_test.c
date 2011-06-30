e03b56dd904e906e42963f836466d08660f8577a
Enno Rehling <enno.rehling@gmail.com>
2011-06-30 13:23:20 -0400

    add tests for get_regions

--- svc/game_test.c
+++ svc/game_test.c
@@ -47,9 +47,38 @@ static void test_add_event_does_not_crash(CuTest * tc)
   CuAssertIntEquals(tc, 0, errno);
 }
 
+static void test_region_iteration(CuTest * tc)
+{
+  struct region * results[4];
+  void * cur;
+  icursor * icur;
+  int i, n = 0;
+
+  svc.reset();
+
+  for (i=0;i!=3;++i) {
+    svc.regions->create(0, i);
+  }
+  cur = svc.get_regions(&icur);
+  CuAssertPtrNotNull(tc, cur);
+  CuAssertPtrNotNull(tc, icur);
+  CuAssertIntEquals(tc, 3, icur->get(cur, 4, (void**)results));
+  for (i=0;i!=3;++i) {
+    int x, y;
+    svc.regions->get_xy(results[i], &x, &y);
+    CuAssertIntEquals(tc, 0, x);
+    n |= (1<<y);
+  }
+  CuAssertIntEquals(tc, 7, n);
+
+  CuAssertIntEquals(tc, 3, icur->advance(&cur, 4));
+  if (icur->destroy) icur->destroy(cur);
+}
+
 void add_game_tests(CuSuite *suite)
 {
   SUITE_ADD_TEST(suite, test_game_reset);
+  SUITE_ADD_TEST(suite, test_region_iteration);
   SUITE_ADD_TEST(suite, test_add_event_does_not_crash);
 }
 
